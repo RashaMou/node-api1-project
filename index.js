b9286b79cfc7a7550b8315a34c9977ad25bb7d2a
Rasha Moumneh <rasha@fastmail.com>
2019-12-03 08:47:08 +0100

    build get users endpoint

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ server.get("/", (req, res) => {
   res.send({ api: "server up and running" });
 });
 
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ errorMessage: "error getting list of users from database" });
+    });
+});
+
+server.post("/api/users");
+
+server.get("/api/users/:id");
+
+server.delete("/api/users/:id");
+
+server.put("/api/users/:id");
+
 const port = 4000;
 server.listen(port, () => {
   console.log(`\n ** API running on port ${port} **\n`);
